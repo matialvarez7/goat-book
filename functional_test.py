4b4670c5f15f8951e82d417f0b08d3cc62298b00
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2024-03-14 00:04:31 -0300

    Added a helper method

--- functional_test.py
+++ functional_test.py
@@ -9,6 +9,12 @@ class NewVisitorTeste(unittest.TestCase):
         self.browser = webdriver.Firefox()
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_todo_list(self):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
@@ -26,18 +32,24 @@ class NewVisitorTeste(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
+        time.sleep(1)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        # self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows), f"New to-do item did not appear in table. Contents were:\n{table.text}",)
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
-        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
+
+         # self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows), f"New to-do item did not appear in table. Contents were:\n{table.text}",)
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+
         # (Edith is very methodical)
         self.fail("Finish the test!")
-        # The page updates again, and now shows both items on her list
         [...]
 
 
