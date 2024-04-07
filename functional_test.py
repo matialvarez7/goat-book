b8b1b915ae8111b2cafc451e0cc8bb0a91031b67
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2024-04-06 21:54:52 -0300

    Model for list Items and associated migration

--- functional_test.py
+++ functional_test.py
@@ -39,6 +39,7 @@ class NewVisitorTeste(unittest.TestCase):
          # self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows), f"New to-do item did not appear in table. Contents were:\n{table.text}",)
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
+        # (Edith is very methodical)
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
@@ -48,7 +49,7 @@ class NewVisitorTeste(unittest.TestCase):
         self.check_for_row_in_list_table("1: Buy peacock feathers")
         self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
-        # (Edith is very methodical)
+        # Satisfied, she goes back to sleep
         self.fail("Finish the test!")
         [...]
 
